4322b0f3a8b989193f5b9fe14a62d2b224c355e7
Sérgio Ferreira <sfer96@gmail.com>
2023-12-12 02:18:47 +0000

    initial commit
    added X and i3 files
    created an automation installation script
    updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# wmde
-Window Managers / Desktop Environments dotfiles and scripts
+* * *
+# wmde #
+
+<p align="center">
+<img alt="Logo" src="assets/banner.jpg" style="width:80%; height:100px;">
+</p>
+
+Window Managers / Desktop Environments configuration files, auxiliary scripts, wallpaper collection
+
+It's like a dotfiles repository for X and graphic systems; for now it's designed for the i3wm + polybar + rofi stack.
+
+* * * 
+
+#### Getting Started ####
+
+Deploy X resource files, like .xinitrc and .Xresources
+
+- ~/.xinitrc: changed to call i3
+- ~/.Xresources: added X font and XTerm settings and a color scheme for all programs that use Xresources.
+- /etc/X11/xorg.conf.d/00-keyboard.conf: keyboard PT [qwerty] for X applications
+
+Deploy i3 configuration files and scripts
+
+
+
+#### Prerequisites ####
+
+.Xresources:
+
+- Mononoki Nerd Font; used in XTerm; may be changed to whatever font you like; to install run:
+
+``` pacman -S --noconfirm --needed ttf-mononoki-nerd  ``` 
+
+
+- 00-keyboard.conf: needs root previleges to copy to /etc/X11/xorg.conf.d
+
+
+#### Installation / Deployment ####
+
+create a Projects directory; cd into it and clone the repository:
+
+``` 
+mkdir -p ~/Projects
+cd ~/Projects
+git clone https://github.com/sergio-a-ferreira/wmde.git
+```
+
+Then cd into the project directory and run install.sh
+
+``` 
+cd wmde
+./install.sh
+```
+
+
+### Authors ####
+
+* **Sergio Ferreira** - *Initial work* - [sergio-a-ferreira](https://github.com/sergio-a-ferreira/wmde.git)
+
+
+#### License ####
+
+This project is licensed under the Unlicense - see the [LICENSE](LICENSE) file for details
---
+++ install.sh
@@ -0,0 +1,237 @@
+#! /usr/bin/env bash
+# ==============================================================================
+# Name        : install.sh
+# Version     : v0.1
+# Description : wmde installation script
+#
+# Arguments   : N/A
+# Returns     : 0 on successfull installation
+#              >0 otherwise
+#
+# Input Files : N/A
+# Output Files: N/A; messages outputed to stderr
+#
+# Notes       : performs simple validations based on heuristics to evaluate
+#                 directory or file deletion.
+#               the files / directories which are marked for deletion are 
+#                 renamed with a suffix .orig_df_<yyyymmddhhmmss>
+#               for example if a file named .config exists it's renamed to 
+#                 .config.orig_yyyymmddhhmmss
+#
+# ==============================================================================
+DOTFILES_DIR="${HOME}/Projects/wmde"
+
+# configure files to deploy here
+# example:
+# [filetype]="action|dotfiles file|/path/to/deploy"
+# action: copy link source
+
+typeset -A file_info=(
+
+	[xinitrc]="link|${DOTFILES_DIR}/X/xinitrc|${HOME}/.xinitrc"
+	[xresources]="link|${DOTFILES_DIR}/X/Xresources|${HOME}/.Xresources"
+	# add new files here
+	#  TODO:: keyboard.conf needs sudo
+	#[keyboard.conf]="copy|${DOTFILES_DIR}/X/00-keyboard.conf|/etc/X11/xorg.conf.d/00-keyboard.conf"
+	[i3wm-config]="link|${DOTFILES_DIR}/i3wm/config|${HOME}/.config/i3/config"
+)
+
+# ==============================================================================
+# - Functions: -----------------------------------------------------------------
+writeMessage(){
+
+	# Description: print a formatted message to stderr
+	# Arguments  : a string with the message
+	# Returns
+	#       - 0  : success
+	#       - >0 : on error
+	# Notes
+	#
+	printf "$0 [$$ :: $(getTimestamp full)]: ${@}\n" 1>&2
+}
+
+
+# ------------------------------------------------------------------------------
+getTimestamp(){
+
+	# Description: format a timestamp with current datetime
+	# Arguments  : full; returns timestamp to nanosecond [yyyy-mm-dd hh:mm:ss.ns]
+	#              y2s: a simple yyyymmddhhmmss for the file renaming
+	# Returns    : formated timestamp
+	# Notes
+	#
+	case "${1}" in
+		"full") date +"%Y-%m-%d %T.%N"
+		;;
+		"y2s") date +"%Y%m%d%H%M%S"
+		;;
+	esac
+}
+
+# ------------------------------------------------------------------------------
+copyFile(){
+
+	# Description: copy a file; if file exists make backup
+	# Arguments  : 1 file origin
+	#              2 file destiny
+	#       - 0  : success
+	#       - >0 : error
+	# Notes      : 
+	#
+
+	typeset origin="${1}"
+	typeset destiny="${2}"
+
+	writeMessage "copying file ${origin} to ${destiny}"
+
+	backUp ${destiny}
+
+	cp -pR ${origin} ${destiny}
+}
+
+# ------------------------------------------------------------------------------
+linkFile(){
+
+	# Description: link a file; if link exists replace it
+	# Arguments  : 1 file origin
+	#              2 file destiny
+	#       - 0  : success
+	#       - >0 : error
+	# Notes      : 
+	#
+
+	typeset origin="${1}"
+	typeset destiny="${2}"
+
+	writeMessage "linking file ${origin} as ${destiny}"
+
+	backUp ${destiny}
+
+	ln -s ${origin} ${destiny}
+}
+
+
+# ------------------------------------------------------------------------------
+backUp(){
+
+	# Description: evaluates if destiny exists and make a back
+	# Arguments  : 1 file destiny
+	#       - 0  : success
+	#       - >0 : error
+	# Notes      : 
+	#
+
+	typeset destiny="${1}"
+
+	if [ -e "${destiny}" ]; then
+		writeMessage "${destiny} exists; replacing with ${origin}"
+		if [ -L ${destiny} ]; then
+			# if link delete
+			rm ${destiny}
+		else
+			# if file / directory make backup
+			writeMessage "backing up ${destiny} as ${destiny}_dt_$(getTimestamp y2s)"
+			cp -pR ${destiny} ${destiny}_dt_$(getTimestamp y2s) && rm -rf ${destiny}
+		fi
+	fi
+}
+
+
+# ------------------------------------------------------------------------------
+sourceFile(){
+
+	# Description: source file <origin> from file <destiny>
+	# Arguments  : 1 file origin
+	#              2 file destiny
+	#       - 0  : success
+	#       - >0 : error
+	# Notes      : 
+	#
+
+	typeset origin=${1}
+	typeset destiny=${2}
+
+	writeMessage "sourcing file ${origin} in ${destiny}"
+
+	typeset msg_id="# source ${origin}"
+	typeset msg_line="[ -r ${origin} ] && . ${origin}"
+
+	if [ -f ${destiny} ]; then
+		grep -q "${msg_id}" ${destiny} && return 0
+	fi
+
+	printf "\n${msg_id}\n${msg_line}\n" >> ${destiny}
+}
+
+# ------------------------------------------------------------------------------
+getInstallInfo(){
+
+	# Description: get file list, origin, destiny and methods for installation
+	# Arguments  : N/A
+	#       - 0  : success
+	#       - >0 : on error
+	# Notes      : 
+	#   don't use tilde expansion
+	#    methods available
+	#      source - source file
+	#      copy   - copy file to destiny
+	#      link   - symlink file in destiny
+
+	for file_type in ${!file_info[@]}; do
+
+		typeset IFS="|"
+		set ${file_info[$file_type]}
+
+		typeset method="${1}"
+		typeset origin="${2}"
+		typeset destiny="${3}"
+
+		case "${method}" in
+			"source") sourceFile "${origin}" "${destiny}"
+			;;
+			"link")	linkFile "${origin}" "${destiny}"
+			;;
+			"copy")	copyFile "${origin}" "${destiny}"
+			;;
+		esac
+	done
+}
+
+# ------------------------------------------------------------------------------
+main(){
+
+	# Description: runtime entry point
+	# Arguments  : N/A; parses ${0} ${@}
+	# Returns    : 
+	#	sucess   : 0
+	#	error    : parses last error received
+	# Notes      : this function is an auxiliary entry point:
+	#                sets process control variables;
+	#                sources scripts and libaries;
+	#                orchestrates the process flow.
+
+	# start process
+	writeMessage "start"
+
+	# get files to install (link or copy)
+	getInstallInfo
+
+	# end process
+	writeMessage "end"
+
+	exit ${rc}
+}
+
+
+# ==============================================================================
+# - Main : ---------------------------------------------------------------------
+# evaluate if script is to be sourced or executed
+if [ "${0}" != "bash" ]; then
+	# run script
+	main
+else
+	# source file; it's an installation script so unset functions and return error
+	unset main writeMessage getTimestamp getInstallInfo sourceFile copyFile linkFile backUp
+	writeMessage "this script is not aimed to be sourced!!"
+	return 130
+fi
